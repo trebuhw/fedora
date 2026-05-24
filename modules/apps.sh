8932519d8cecde62f8228ec849ba9776c988ebea
trebuhw <114191788+trebuhw@users.noreply.github.com>
2026-05-24 12:25:05 +0200

    up

---
+++ modules/apps.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Programy użytkowe
+dnf install -y \
+  bat \
+  blueman \
+  btop \
+  chromium \
+  codium \
+  eza \
+  fastfetch \
+  file-roller \
+  fish \
+  ghostty \
+  gthumb \
+  htop \
+  i3lock \
+  iwd \
+  lm_sensors \
+  ncdu \
+  neovim \
+  network-manager-applet \
+  nwg-look \
+  onedrive \
+  pamixer \
+  pavucontrol \
+  scrot \
+  speedtest-cli \
+  starship \
+  sxiv \
+  Thunar \
+  thunar-archive-plugin \
+  thunar-volman \
+  thunderbird \
+  tldr \
+  trash-cli \
+  tumbler \
+  ueberzugpp \
+  vim-enhanced \
+  yazi \
+  zathura \
+  zathura-pdf-poppler
